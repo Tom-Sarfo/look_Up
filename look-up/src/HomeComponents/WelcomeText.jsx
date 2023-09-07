21055a3d363ea8d1166604c47ffa1bad4498609b
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-09-07 10:58:02 +0000

    Display Welcome text Conditionaly by checking empty input

--- look-up/src/HomeComponents/WelcomeText.jsx
+++ look-up/src/HomeComponents/WelcomeText.jsx
@@ -1,3 +1,15 @@
+import { useContext } from "react";
+import { DrugData } from "../DrugDataContext";
+
 export default function WelcomeText() {
-	return <div>WelcomeText</div>;
+	const { input } = useContext(DrugData);
+	const [searchInput] = input;
+
+	return (
+		<div>
+			{searchInput === "" && (
+				<div>Get Quick Information on your Stock Position!</div>
+			)}
+		</div>
+	);
 }
