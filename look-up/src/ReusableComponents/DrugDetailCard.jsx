42cffaac67ea36893eefa57a7dc9f5e15ce3c600
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-09-08 09:22:10 +0000

    Fix Paper import from Mui DrugDetailCard.jsx

--- look-up/src/ReusableComponents/DrugDetailCard.jsx
+++ look-up/src/ReusableComponents/DrugDetailCard.jsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Paper } from "@mui/material/Paper";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import { useState, useContext } from "react";
